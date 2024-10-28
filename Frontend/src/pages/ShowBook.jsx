691146f42946a7f73987f494b3b495e864a7813f
YASHWANTH2601 <110809712+YASHWANTH2601@users.noreply.github.com>
2024-10-28 15:26:56 +0530

    Update ShowBook.jsx

--- Frontend/src/pages/ShowBook.jsx
+++ Frontend/src/pages/ShowBook.jsx
@@ -11,7 +11,7 @@ export default function ShowBook() {
 
   useEffect(() => {
     axios
-      .get("http://localhost:5555/books/" + id)
+      .get("https://book-store-backend-8mlt.onrender.com/" + id)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
